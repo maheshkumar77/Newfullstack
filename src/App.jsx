b35ec13140c4e48e468afe17f1e161bdcea46f3e
maheshkumar77 <149643073+maheshkumar77@users.noreply.github.com>
2025-06-02 23:51:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+       <h1 
+                
+                 className="pb-16 text-6xl  font-thin tracking-tight lg:mt-16 lg:text-8xlbg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text  text-transparent">Mahesh Ku. Sahu</h1>
+                
+    </>
+  )
+}
+
+export default App
